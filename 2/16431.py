c2584e88cdfa8c967805985d94f31a7e047a48ad
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-01-12 01:53:02 +0400

    nthng

--- 2/16431.py
+++ 2/16431.py
@@ -1,5 +1,3 @@
-# ((y → x) ≡ (x → w)) ∧ (z ∨ x).
-
 print('x', 'y', 'z', 'w')
 
 for x in (0, 1):
@@ -9,5 +7,3 @@ for x in (0, 1):
                 if ((y <= x) == (x <= w)) and (z or x):
                     print(x, y, z, w)
 
-
-#  1-y  2-w 3-x  4-z
