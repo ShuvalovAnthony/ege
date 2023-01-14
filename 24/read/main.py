f34e0282ab4e0443e5496599a5509b8d0d121374
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-01-14 19:31:22 +0400

    fix num issue

---
+++ 24/read/main.py
@@ -0,0 +1,18 @@
+f = open('24/read/text.txt')
+
+
+# print(f.read()) # использует весь файл
+
+print(f.readline())
+print(f.readline())
+print(f.readline())
+
+print(f.readline())
+print(f.readline())
+
+# print(f.read())
+
+
+
+
+# print(f.readlines())
