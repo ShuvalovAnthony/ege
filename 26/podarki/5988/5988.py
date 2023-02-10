fe7a46a232eb24d3387889c6fc7383e0d017fe5f
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-02-10 20:03:11 +0700

    upd

--- 26/podarki/5988/5988.py
+++ 26/podarki/5988/5988.py
@@ -29,6 +29,7 @@ for i in data:
 
 answ = [[0, 0] for i in range(len(uniq_data))]
 
+
 for i in range(len(uniq_data)):
     max_side_len = 10**6
     max_count = 0
@@ -44,4 +45,27 @@ for i in range(len(uniq_data)):
         max_side_len =  uniq_data[i][0]
     answ[i] = [max_count + 1, max_side_len]
 
-print(answ[-1])
+print(*answ[-1])
+
+
+
+# # ЖАДНЫЙ АЛГОРИТМ (НЕПРАВИЛЬНЫЙ!!!) - 1 балл из 2х за задачу
+
+# max_side_len = 0
+# max_count = 0
+
+# for i in range(len(uniq_data)):
+#     temp_count = 1
+#     last_side = uniq_data[i]
+
+#     for j in range(i + 1, len(uniq_data) - 1):
+#         if (uniq_data[j][0] >= last_side[0] + 7) and \
+#             (last_side[1] != uniq_data[j][1]):
+#                 temp_count += 1
+#                 last_side = uniq_data[j]
+    
+#     if temp_count > max_count:
+#         max_count = temp_count
+#         max_side_len = uniq_data[i][0]
+
+# print(max_count, max_side_len)
