d230e22fed8a3e9cb9005fdf1c344f803dc9c5bd
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-02-15 22:01:56 +0700

    upd

---
+++ 15/5923.py
@@ -0,0 +1,31 @@
+from itertools import combinations
+
+def p(x):
+    return 5 <= x <= 280
+
+def q(x):
+    return 295 <= x <= 400
+
+def r(x):
+    return 375 <= x <= 450
+
+def a(start, stop, x):
+    return start <= x <= stop
+
+
+coords = combinations([5, 280, 295,375, 400, 450], 2)
+
+min_dlina = 10**6
+
+for start, stop in coords:
+    flag = True
+
+    for x in range(0, 1000):
+        if not ((q(x) <= p(x)) or ((not a(start, stop, x)) <= r(x))):
+            flag = False
+            break
+
+    if flag:
+        min_dlina = min(min_dlina, stop - start)
+
+print(min_dlina)
