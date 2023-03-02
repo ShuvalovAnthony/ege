cf02c35bdb56ac49f59edc3897ad598f7b3a44d6
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-03-02 22:01:56 +0700

    upd

--- 27__/3294/3294.py
+++ 27__/3294/3294.py
@@ -1,5 +1,19 @@
-f = open('27__/3294/27-A.txt')
+f = open('27__/3294/27-B.txt')
 
 n = int(f.readline())
 nums = [int(i) for i in f]
 
+
+def count_pairs(arr, n):
+    count = 0
+    for i in range(n):
+        for j in range(i+6, n):
+            if (arr[i] + arr[j])%2 and arr[i]*arr[j]%13 == 0:
+                count += 1
+        if i%100 == 0:
+            print(i)
+    return count
+
+
+
+print(count_pairs(nums, n))
