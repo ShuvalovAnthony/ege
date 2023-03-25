ce426735dbc6fe0796042678e866a36116804ab7
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-03-25 23:01:44 +0700

    upd

---
+++ theory/1.py
@@ -0,0 +1,58 @@
+# # Палиндром - слово или число которое слева направо читается также
+# # как и справа налево
+
+
+# def palindrom(num):
+#     return str(num) == str(num)[::-1]
+
+
+# vvod = input()
+
+# slova = vvod.split()
+# print(slova)
+
+# slova_s_a = []
+
+# for slovo in slova:
+#     slova_s_a.append(slovo + "A")
+
+
+
+# print('____RAZDELITEL___'.join(slova_s_a))
+
+# print("hello", "world", "!", 5345, "fdsgdfg")
+
+
+# print(' '.join([str(i) for i in range(1, 100, 2)]))
+
+
+# res = ''
+
+# for i in range(1, 100, 2):
+#     res += str(i) + ' '
+
+# print(res)
+
+
+
+
+# def plus_five(num):
+#     return num + 5
+
+
+# nums = [1, 423, 5, 12, -123, 0]
+
+# nums = list(map(lambda x: x + 5, nums))
+
+
+
+goods = [20, 10, 40, 30]
+
+for i in sorted(goods): # 20          10 40 30 -> 10 20 30 40
+        
+    print(i)
+
+
+
+
+
