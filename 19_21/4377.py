d06ac3eeb167c77c2843e5b4a6f01d580d8e4997
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-04-05 21:47:59 +0700

    upd

--- 19_21/4377.py
+++ 19_21/4377.py
@@ -1,6 +1,9 @@
 def f(s, step=1): #1p 2v 3p 4v 5p
-    if (step == 5) and (s >= 26): return 1
-    elif (step == 5) and (s < 26): return 0
+    if (step in (3, 5)) and (s >= 26): return 1
+    elif (
+        ((step == 5) and (s < 26)) or
+        (s >= 26)
+        ): return 0
     if s%2:
         if step%2: # petya
             return f(s + 1, step + 1) and\
