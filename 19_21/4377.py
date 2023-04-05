4c9ece595cc0cf9e6ff0a6cdb69b767b1b553ac9
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-04-05 22:13:13 +0700

    upd

--- 19_21/4377.py
+++ 19_21/4377.py
@@ -1,4 +1,4 @@
-def f(s, step=1): #1p 2v 3p 4v 5p
+def f19(s, step=1): #1p 2v 3p 4v 5p
     if (step in (3, 5)) and (s >= 26): return 1
     elif (
         ((step == 5) and (s < 26)) or
@@ -6,21 +6,51 @@ def f(s, step=1): #1p 2v 3p 4v 5p
         ): return 0
     if s%2:
         if step%2: # petya
-            return f(s + 1, step + 1) and\
-            f(s + 2, step + 1) and\
-            f(s*2, step + 1)
+            return f19(s + 1, step + 1) and\
+            f19(s + 2, step + 1) and\
+            f19(s*2, step + 1)
         else:
-            return f(s + 1, step + 1) or\
-            f(s + 2, step + 1) or\
-            f(s*2, step + 1)
+            return f19(s + 1, step + 1) or\
+            f19(s + 2, step + 1) or\
+            f19(s*2, step + 1)
     else:
         if step%2: # petya
-            return f(s + 1, step + 1) and\
-            f(s + 2, step + 1)
+            return f19(s + 1, step + 1) and\
+            f19(s + 2, step + 1)
         else:
-            return f(s + 1, step + 1) or\
-            f(s + 2, step + 1)
+            return f19(s + 1, step + 1) or\
+            f19(s + 2, step + 1)
         
 
 for s in range(1, 26):
-    if f(s): print(s)
+    if f19(s): print(s)
+
+
+print('-'*20)
+
+def f20(s, step=1): # 1p 2v 3p 4v 5p
+    if (step == 4) and (s >= 26): return 1
+    elif (
+        ((step == 4) and (s < 26)) or
+        (s >= 26)
+        ): return 0
+    if s%2:
+        if step%2 == 0: # petya
+            return f20(s + 1, step + 1) and\
+            f20(s + 2, step + 1) and\
+            f20(s*2, step + 1)
+        else:
+            return f20(s + 1, step + 1) or\
+            f20(s + 2, step + 1) or\
+            f20(s*2, step + 1)
+    else:
+        if step%2 == 0: # petya
+            return f20(s + 1, step + 1) and\
+            f20(s + 2, step + 1)
+        else:
+            return f20(s + 1, step + 1) or\
+            f20(s + 2, step + 1)
+        
+
+for s in range(1, 26):
+    if f20(s): print(s)
