2872acf0f0244c1de1fb865d418fb912cf7717ef
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-04-27 00:16:04 +0700

    upd

--- 13/6020.py
+++ 13/6020.py
@@ -2,6 +2,10 @@
 s = "АБГВ БГД ВГЗ ГДЕЗ ДИЕ ЕИК ЗЛ ИМК КМ ЛКМ М"
 d = {i[0]:i[1:] for i in s.split()}
 
+
+# def f(a, b):
+#     return (a == b) + sum(f(c, b) for c in d[a])
+
 f = lambda a, b: (a == b) +\
       sum(f(c, b) for c in d[a])
 
