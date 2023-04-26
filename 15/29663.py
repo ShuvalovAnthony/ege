572e09c2ec1a954c5e8aca4666bb7b28980a8340
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-04-26 22:09:45 +0700

    upd

--- 15/29663.py
+++ 15/29663.py
@@ -3,7 +3,6 @@ for a in range(10**4, 1, -1):
     for x in range(1, 1000):
         if not ((a < 50) and ((x%a != 0) <= ((x%10 == 0) <= (x%12 != 0)))):
             flag = False
-    print(a)
     if flag:
         print('OTVET', a)
         break
