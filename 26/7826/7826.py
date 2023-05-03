b09cd1a672a07ac58c12f9c6e530c732bdec9b6e
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-05-03 17:34:24 +0700

    upd

---
+++ 26/7826/7826.py
@@ -0,0 +1,28 @@
+f = open('26/7826/26_7826.txt')
+
+k, n = [int(i) for i in f.readline().split()]
+
+visitors = sorted([list(map(int, i.split())) for i in f],  key=lambda x: (x[0], -x[1]))
+
+current_time = 0
+
+stops = [-1]*k
+visitors_counter = 0
+
+last_attrs_number = 0
+
+last_start = 0
+
+for start, stop in visitors:
+    for attr_index in range(k):
+        if start == last_start:
+            visitors_counter += 1
+            break
+        if start >= stops[attr_index]:
+            visitors_counter += 1
+            stops[attr_index] = stop + 1
+            last_attrs_number = attr_index + 1
+            last_start = start
+            break
+
+print(visitors_counter, last_attrs_number)
