2e7a8b61a67399e351ee33a8dc4cac2fca442af4
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-06-16 22:28:19 +0700

    upd

---
+++ 16/3033.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+@lru_cache
+def f(n):
+    if n == 0: return 1
+    if n%2: return 1 + f(n - 1)
+    return f(n//2)
+
+
+
+counter = 0
+
+for i in range(1, 5*10**8 + 1):
+    if f(i) == 3: counter += 1
+    print(i)
+
+
+print(counter)
