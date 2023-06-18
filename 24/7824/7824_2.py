b1e56fa2556035ab99eff1a361fe3f51082f92d1
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-06-19 00:05:32 +0700

    upd

--- 24/7824/7824_2.py
+++ 24/7824/7824_2.py
@@ -17,3 +17,20 @@ for i in range(len(f)):
         tmp_count = 0
 
 print(count)
+
+
+
+
+
+
+
+count = 1
+max_c = -10**6
+for i in range(len(f) - 2):
+    if (f[i] + f[i + 1] + f[i + 2]) not in combos:
+        count += 1
+    else:
+        max_c = max(max_c, count)
+        count = 1
+
+print(max_c)
