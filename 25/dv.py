d56d95aeb98095e7a73632abdc1db50d2d8e3652
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-06-19 12:29:19 +0700

    upd

--- 25/dv.py
+++ 25/dv.py
@@ -1,8 +1,19 @@
 from fnmatch import fnmatch
 
 
-for i in range(2031, 10**9 + 1, 2031):
+def check(num):
+    kolvo_delitelei = 0
+    for i in range(1, num + 1):
+        if num%i == 0: kolvo_delitelei += 1
+    for stepen_dvoiki in range(31):
+        if 2**stepen_dvoiki == kolvo_delitelei: return True
+    return False
+
+
+
+for i in range(31*2031, 10**9 + 1, 31*2031):
     if fnmatch(
         str(i), '*31*65'
-    ) and i%31 == 0:
-        print(i, i//2031)
+    ):
+        if check(i):
+            print(i, i//2031)
