cb569dd7d7e920594a75d76956b26093cec73fef
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-07-30 23:35:51 +0700

    upd

--- 8/10473_.py
+++ 8/10473_.py
@@ -1,10 +1,7 @@
+from itertools import product
+
 count = 0
-for n1 in '1234':
-    for n2 in '1234':
-        for n3 in '1234':
-            for n4 in '1234':
-                for n5 in '1234':
-                    res = n1 + n2 + n3 + n4 + n5
-                    if res.count('1') == 2:
-                        count += 1
+for i in product('1234', repeat=5):
+    if i.count('1') == 2:
+        count += 1
 print(count)
