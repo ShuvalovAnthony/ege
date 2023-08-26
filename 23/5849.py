0ac1a6a25728642d0e923da07189c129bce3bc36
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-08-27 00:00:51 +0700

    upd

--- 23/5849.py
+++ 23/5849.py
@@ -1,13 +1,9 @@
-from functools import lru_cache
-
-
-@lru_cache
 def f(a, b):
     if a > b: return 0
     elif a == b: return 1
-    c = str(a)
-    c = int(str(int(c[0]) + 1) + c[1::])
+    # c = str(a)
+    # c = int(str(int(c[0]) + 1) + c[1::])
     return f(a + 1, b) + f(a + 10, b)
 
 
-print(f(35, 301))
+print(f(35, 57))
