956d14a6c7a4de804c7af895c95e1df7ce27746b
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-08-27 09:04:56 +0700

    upd

--- 12/5062.py
+++ 12/5062.py
@@ -1,36 +1,20 @@
-from itertools import product
-from functools import lru_cache
-
-
-@lru_cache
 def algo(stroka: str):
     while '00' not in stroka:
         stroka = stroka.replace('011', '20', 1)
         stroka = stroka.replace('022', '10', 1)
-        stroka = stroka.replace('02', '110', 1)
-        stroka = stroka.replace('01', '220', 1)
+        stroka = stroka.replace('01', '220', 1) # опечатка!! поменять
+        stroka = stroka.replace('02', '110', 1) # местами эти строки
     return stroka
 
 
-@lru_cache
-def check(stroka: str):
-    return stroka.count('1') == stroka.count('2')
+for i in range(1, 100):
+    for j in range(1, 100):
+        s = '0' + '1221'*i + '12'*j + '0'
 
+        s = algo(s)
 
-@lru_cache
-def check_answ(stroka: str):
-    return (stroka.count('1') == 47) and (stroka.count('2') < 70)
-
-
-for i in range(50):
-    print(i)
-    for stroka_ in product('12', repeat=i*2):
-        stroka = '0' + ''.join(stroka_) + '0'
-        if check(stroka):
-            result_stroka = algo(stroka)
-            if check_answ(result_stroka): 
-                print(result_stroka.count('2'))
-            
-            
+        if (s.count('1') == 47 and s.count('2') < 70):
+            print(s.count('2'), i, j)
+                
 
 
