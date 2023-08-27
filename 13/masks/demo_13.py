eaeb31844da578334affe98abc61fa608d7535a8
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-08-28 01:15:58 +0700

    upd

--- 13/masks/demo_13.py
+++ 13/masks/demo_13.py
@@ -1,12 +1,13 @@
-from ipaddress import IPv4Network
+import ipaddress
 
-network_address = IPv4Network('192.168.32.160/255.255.255.240')
+adresses = ipaddress.IPv4Network('192.168.32.160/255.255.255.240')
 
-count = 0
-for ip in network_address:
-    binary_ip = bin(int(ip))[2:]  # Преобразуем IP-адрес в двоичное представление
-    ones_count = binary_ip.count('1')  # Считаем количество единиц в двоичном представлении
-    if ones_count%2 == 0:  # Проверяем, чётное ли количество единиц
-        count += 1
 
-print(count)
+counter = 0
+
+for adress in adresses:
+    bin_adress = bin(int(adress))[2:]
+    if bin_adress.count('1')%2 == 0: counter += 1
+
+
+print(counter)
