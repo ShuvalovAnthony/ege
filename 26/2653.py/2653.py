ba527f90109a0490e9f86715e8a2b6dd6f893588
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-08-28 09:49:02 +0700

    upd

--- 26/2653.py/2653.py
+++ 26/2653.py/2653.py
@@ -1,24 +1,9 @@
 f = open('26/2653.py/26_2653.txt')
 
 n = int(f.readline())
+s = sorted([int(f.readline()) for i in range(n)])
 
-pos_sum = set() # возможные суммы
-
-weights = [int(i) for i in f]
-
-for weight in weights:
-    [pos_sum.add(summa + weight) for summa in pos_sum.copy()]
-    pos_sum.add(weight)
-
-max_impossible_summ = 0
-
-for i in range(sum(weights), 0, -1): # идем от самого большого числа
-    if i not in pos_sum:            # если такого числа нет в
-        max_impossible_summ = i     # списке возможных
-        break                       # сохраняем его и оставнавливаемся
-
-# sum(weights) - (len(pos_sum)) - количество весов, которые
-# нельзя набрать (максимальный вес как сумма всех гирь минус
-#                 количество возможных наборов весов)
-
-print(sum(weights) - (len(pos_sum)), max_impossible_summ)
+a = {0}
+for i in s: a |= {x + i for x in a}
+ans = [i for i in range(sum(s)) if i not in a]
+print(len(ans), max(ans))
