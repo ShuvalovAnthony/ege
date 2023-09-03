ad9aac4a2341e6b6887739cd74e8aba51d0cdc10
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-09-04 00:22:49 +0700

    upd

--- 10/6761/6761.py
+++ 10/6761/6761.py
@@ -2,13 +2,14 @@ from string import punctuation
 
 f = open('10/6761/6761.txt').read()
 
-counter = 0
-
+# удаляем все знаки препинания
 for i in punctuation:
     f = f.replace(i, '')
 
-for word in f.lower().split():
-    if word in ('как', 'что'):
+counter = 0
+
+for word in f.lower().split(): # f.lower() - все маленькими (чтобы игнорировать регистр) .split() - список слов
+    if word in ('как', 'что'): # word == 'как' or word == 'что'
         counter += 1
 
 print(counter)
