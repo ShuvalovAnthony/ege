bdc808f3b47ee360545050d64fa48bc0f29c1b01
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-09-29 22:39:29 +0700

    upd

--- 17/39763/39763.py
+++ 17/39763/39763.py
@@ -9,9 +9,8 @@ count = 0
 max_summa = 0
 
 for i in range(len(nums) - 2):
-    if ((nums[i]**2 + nums[i + 1]**2 == nums[i + 2]**2) and
-    (nums[i + 1]**2 + nums[i + 2]**2 == nums[i]**2) and
-    (nums[i + 2]**2 + nums[i]**2 == nums[i + 1]**2)):
+    sides = sorted([nums[i], nums[i + 1], nums[i + 2]])
+    if (sides[-1]**2 < sides[0]**2 + sides[1]**2):
         count += 1
         max_summa = max(max_summa, sum([nums[i], nums[i + 1], nums[i + 2]]))
 
