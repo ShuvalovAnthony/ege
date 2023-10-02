3c57524b00bd5b43fd00e9c2ad5fef94599a3868
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-10-02 23:43:43 +0700

    upd

--- 14/48391.py
+++ 14/48391.py
@@ -1,24 +1,10 @@
-def convert(num: str, from_base: int, to_base: int):
-		num = int(str(num), from_base)
-		alphabet = '0123456789ABCDEFGHIJKLMNOPQ'
-		answ = alphabet[num%to_base]
-		while num >= to_base:
-				num //= to_base
-				answ += alphabet[num%to_base]
-		return answ[::-1]
-
-
-
-
-# yAAx.12 + x02y.14
-
 alph = '0123456789AB'
 
 for x in alph:
     for y in alph:
         first_num = y + "AA" + x
         second_num = x + "02" + y
-        res = int(convert(first_num, 12, 10)) + int(convert(second_num, 14, 10))
+        res = int(first_num, 12) + int(second_num, 14)
         if res%80 == 0:
             print(res/80)
             
---
+++ 24/59847/59847.py
@@ -0,0 +1,29 @@
+f = open('24/59847/24.txt').read()
+
+
+
+dlini = []
+temp_dlina = 0
+
+
+for i in range(len(f) - 1):
+    if f[i] + f[i + 1] == 'WW':
+        dlini.append(temp_dlina)
+        temp_dlina = 0
+    else:
+        temp_dlina += 1
+
+# print(dlini)
+
+
+max_dlina = 0
+
+for i in range(len(dlini) - 99):
+    print(sum(dlini[i:i + 99]))
+    max_dlina = max(
+        max_dlina,
+        sum(dlini[i:i + 99])
+    )
+
+
+print(max_dlina + 200)
