a76db9d9c9eda3f9c056db5ce0dc161c4a680e33
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-11-01 16:34:41 +0700

    upd

--- 9/59834/59834.py
+++ 9/59834/59834.py
@@ -7,11 +7,9 @@ def check(nums):
     if len(set(nums)) != 5: return False
 
     summa_povtor = 0
-    summa_nepovtor = 0
     for num in nums:
         if nums.count(num) == 2: summa_povtor += num
-        else: summa_nepovtor += num
-    return summa_povtor < summa_nepovtor/4
+    return summa_povtor < (sum(nums) - summa_povtor)/4
 
 counter = 0
 
