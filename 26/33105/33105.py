bbc228f4c41988107e0668267ca0788f1849bcab
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-11-23 21:06:39 +0700

    upd

--- 26/33105/33105.py
+++ 26/33105/33105.py
@@ -2,28 +2,20 @@ f = open('26/33105/inf_22_10_20_26.txt')
 
 n = int(f.readline())
 
-
 itog_summa = 0
 
 prices = [] # список товаров дороже 100 рублей
 
 for i in f:
     price = int(i)
-    if price <= 100:
-        itog_summa += price
-    else:
-        prices.append(price)
+    if price <= 100: itog_summa += price
+    else: prices.append(price)
 
 prices = sorted(prices)
 
-if len(prices)%2 == 0:
-    prices_skidka = prices[:len(prices)//2]
-    prices_bez_skidki = prices[len(prices)//2:]
-else:
-    itog_summa += prices[len(prices)//2]
-    prices_skidka = prices[:len(prices)//2]
-    prices_bez_skidki = prices[len(prices)//2 + 1:]
 
+prices_skidka = prices[:len(prices)//2]
+prices_bez_skidki = prices[len(prices)//2:]
 
 itog_summa += sum(prices_skidka)*0.7 + sum(prices_bez_skidki)
 
