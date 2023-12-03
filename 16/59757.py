81fb6a024a8673b064bfb4ded2bc0253b33df5fb
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2023-12-03 17:44:02 +0700

    upd

---
+++ 16/59757.py
@@ -0,0 +1,34 @@
+from functools import lru_cache
+import sys
+
+
+sys.setrecursionlimit(5000)
+
+
+@lru_cache
+def f(n):
+    if n < 11: return 10
+    return n + f(n - 1)
+
+
+print(f(2024) - f(2022))
+
+
+counter = 0
+
+def f(n):
+    if n == 0: return 0
+    if n%2 == 0: return f(n/2)
+    return 1 + f(n - 1)
+
+
+def check(n):
+    return f(n) == 3
+
+
+for n in range(1, 1000 + 1):
+    counter += check(n)
+
+
+print(counter)
+
