6ed8c3ca070e5e547fd8058e89745c625aca1ae0
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-01-14 16:29:28 +0700

    upd

---
+++ 9/63058/63058.py
@@ -0,0 +1,28 @@
+f = open('9/63058/63058.txt')
+
+nums = [[int(i) for i in row.split()] for row in f]
+
+
+def check(row):
+    if len(set(row)) == len(row): return False
+    row = sorted(row)
+
+    summa_povtor = 0
+    for num in row:
+        if row.count(num) > 1:
+            summa_povtor += num
+
+    if (
+        (row[-1] != row[-2]) and
+        (summa_povtor < row[-1])
+    ): return True
+    return False
+
+
+kolichestvo = 0
+
+for row in nums:
+    if check(row):
+        kolichestvo += 1
+
+print(kolichestvo)
