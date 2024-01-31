0b2708c011939c1a0e32bf1c68533147e5b859d5
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-01-31 22:42:29 +0700

    upd

--- 8/15920.py
+++ 8/15920.py
@@ -1,13 +1,26 @@
+# alph = sorted('АЛГОРИТМ')
+
+# counter = 0
+
+# for a1 in alph:
+#     for a2 in alph:
+#         for a3 in alph:
+#             for a4 in alph:
+#                 word = a1 + a2 + a3 + a4
+#                 counter += 1
+#                 if word[0] == 'Г' and word[1] == 'О':
+#                     print(counter)
+                    
+
+
+from itertools import product
+
 alph = sorted('АЛГОРИТМ')
 
 counter = 0
 
-for a1 in alph:
-    for a2 in alph:
-        for a3 in alph:
-            for a4 in alph:
-                word = a1 + a2 + a3 + a4
-                counter += 1
-                if word[0] == 'Г' and word[1] == 'О':
-                    print(counter)
-                    
+for word in product(alph, repeat=4):
+    counter += 1
+    if word[0] == 'Г' and word[1] == 'О':
+        print(counter)
+        break
