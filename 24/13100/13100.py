12400777c831116c95106535dd78c1225a22a47a
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-02-12 00:39:12 +0700

    upd

--- 24/13100/13100.py
+++ 24/13100/13100.py
@@ -1,14 +1,11 @@
 f = open("24/13100/24_13100.txt").read()
 
 
-index_c = []
-index_d = []
+all_indexes = []
 
 for i in range(len(f)):
-    if f[i] == 'C': index_c.append(i)
-    if f[i] == 'D': index_d.append(i)
+    if (f[i] == 'C') or (f[i] == 'D'): all_indexes.append(i)
 
-all_indexes = sorted(index_c + index_d)
 
 
 def check_temp(temp_posled):
@@ -17,7 +14,7 @@ def check_temp(temp_posled):
 
 max_dlina = 0
 
-for i in range(len(all_indexes) - 6): # 0    1 2 3 4   5
+for i in range(len(all_indexes) - 5): # 0    1 2 3 4   5
     temp_posled = f[all_indexes[i] + 1: all_indexes[i+5]]
     if check_temp(temp_posled):
         left = all_indexes[i]
