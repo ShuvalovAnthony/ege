ca6973e6b32d834cc1945ddc171e1e26f8570d3a
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-02-18 16:59:34 +0700

    upd

---
+++ 24/58328/58328.py
@@ -0,0 +1,17 @@
+from string import digits
+
+f = open("24/58328/24_58328.txt").read()
+
+
+for digit in digits:
+    for i in range(30, 1, -1):
+        f = f.replace(digit*i, " ")
+
+
+print(
+    max(
+        [len(i) for i in f.split()]
+    ) + 2
+)
+    
+
