25abb84d19533ed82253fd92d92b3d5deb1485f7
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-03-18 01:00:56 +0700

    udpasdasd

--- 24/13085/13085.py
+++ 24/13085/13085.py
@@ -5,10 +5,13 @@ for i in range(len(f)):
     if (f[i] == 'X') or (f[i] == 'Y'):
         indexes.append(i)
 
+max_len = 0
+
 for i in range(len(indexes) - 4):
     kusok_i_bokovuhi = f[indexes[i]:indexes[i + 3] + 1]
     kusok_bez_bokovuh =  f[indexes[i] + 1:indexes[i + 3]]
     if kusok_bez_bokovuh.count('X') == kusok_bez_bokovuh.count('Y'):
-        print(indexes[i + 3] - indexes[i] - 1)
+        max_len = max(max_len, len(kusok_bez_bokovuh))
 
 
+print(max_len)
