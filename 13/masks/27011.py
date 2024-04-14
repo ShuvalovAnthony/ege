bf16d5eb15bf26042c691cf2f44ec7992d7aca07
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-04-14 18:02:58 +0700

    upd

---
+++ 13/masks/27011.py
@@ -0,0 +1,6 @@
+print([bin(int(i))[2:].zfill(8) for i in "192.168.87.35".split(".")])
+print([bin(int(i))[2:].zfill(8) for i in "84.77.96.123".split(".")])
+
+
+print(bin(95)[2:].zfill(8))
+print(bin(96)[2:].zfill(8))
