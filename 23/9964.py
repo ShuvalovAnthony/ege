a31ea8fa049035b12e891bd2cb1e5151d9817a06
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-04-26 20:45:03 +0700

    upd

--- 23/9964.py
+++ 23/9964.py
@@ -1,16 +1,17 @@
-# from functools import lru_cache
+from functools import lru_cache
 
+@lru_cache(None)
+def f2(s, e, p1='', p2='', valid=False):
+    if s > e: return 0
+    if s == e: return valid
 
-# @lru_cache
-# def f(start, stop):
-#     if start == stop: return True
-#     elif start > stop: return False
+    count = 0
+    count += f2(s+1, e, 'A', p1, valid)
+    count += f2(s*3, e, 'B', p1, valid)
 
-#     return (
-#         f(start + 1, stop)
-#         + f(start*3, stop)
-#         + f(start + 5, stop)
-#     )
+    valid = valid or (p2 + p1 + 'C' == 'CAC')
+    count += f2(s+5, e, 'C', p1, valid)
 
+    return count
 
-# count = 0
+print(f2(3, 69))
