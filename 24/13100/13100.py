c73eabeeb6f3eeb956403788cf4b540bb411e774
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-05-19 22:46:21 +0700

    upd

--- 24/13100/13100.py
+++ 24/13100/13100.py
@@ -15,14 +15,10 @@ max_dlina = 0
 for i in range(len(all_indexes) - 5): # 0    1 2 3 4   5
     temp_posled = f[all_indexes[i] + 1: all_indexes[i+5]]
     if check_temp(temp_posled):
-        left = all_indexes[i]
-        right = all_indexes[i + 6]
-        stroka = f[all_indexes[i] + 1: all_indexes[i+5]]
-        temp_dlina = len(stroka)
-
-        if temp_dlina > max_dlina:
-            max_dlina = temp_dlina
-            answ = stroka
+        
+        if len(temp_posled) > max_dlina:
+            max_dlina = len(temp_posled)
+            answ = temp_posled
 
 
 print(max_dlina)
