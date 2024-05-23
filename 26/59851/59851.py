55b5f914594ef7f5636c3fa17021bc474f79f38a
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2024-05-23 22:58:22 +0700

    gavno sobache

---
+++ 26/59851/59851.py
@@ -0,0 +1,20 @@
+f = open("26/59851/26_6759.txt")
+
+n = int(f.readline())
+
+prices = sorted([int(i) for i in f], reverse=True)
+# n = 7
+
+# prices = [7, 6, 5, 4, 3, 2, 1]
+
+# prices = [7, 5, 4,    6, 3, 2,      1] -> [, 5, 4, , 3, 2, 1]
+
+# prices = [7, 5, 3,      6,  4,  2,          1] 
+
+len_discount = n//3
+
+plan = sum(prices[len_discount:])
+fact = sum(prices) - sum(prices[2::3])
+
+
+print(plan, fact)
---
+++ 26/68527/68527.py
@@ -0,0 +1,16 @@
+f = open("26/68527/1_26.txt")
+
+n = int(f.readline())
+
+nums = sorted([int(i) for i in f], reverse=True)
+
+max_kolvo_korzhei = 1
+last_korzh = nums[0]
+
+for num in nums[1:]:
+    if num <= last_korzh - 4:
+        max_kolvo_korzhei += 1
+        last_korzh = num
+
+
+print(max_kolvo_korzhei, last_korzh)
