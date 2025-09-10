b14c3b957a35ae8b703d9916f011504518b96f47
ShuvalovAnthony <anthony.shuvalov@mail.ru>
2025-09-11 00:46:50 +0700

    upd

--- 2/28538.py
+++ 2/28538.py
@@ -7,6 +7,7 @@ def f(x, y, z, w):
 
 for a in product([0, 1], repeat=6):
     t = [(1, a[0], 1, 1), (0, a[1], a[2], 0), (1, a[3] , a[4], a[5])]
+    
     if len(t) == len(set(t)):
         for p in permutations('xywz'):
             if [f(**dict(zip(p, r))) for r in t] == [0, 0, 0]:
